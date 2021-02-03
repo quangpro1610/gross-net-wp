e4c066d5eaa748758d2e234e6fc1285c438509d3
VINADES.,JSC <contact@vinades.vn>
2021-02-03 13:40:38 +0700

    Cập nhật trang settings

--- gross-net.php
+++ gross-net.php
@@ -2,8 +2,8 @@
 /**
  * Plugin Name:       GrossNet
  * Plugin URI:        https://codedaokysu.com
- * Description:       Công cụ tính lương Gross sang Net / Net sang Gross chuẩn 2020
- * Version:           1.0
+ * Description:       Công cụ tính lương Gross sang Net / Net sang Gross chuẩn 2021
+ * Version:           1.0.0
  * Author:            Quang Hoang
  * Author URI:        https://codedaokysu.com
  * Text Domain:       gross-net
@@ -11,3 +11,12 @@
 require_once __DIR__ . '/includes/grossnet-functions.php';
 require_once __DIR__ . '/includes/grossnet-ajax.php';
 
+if ( !function_exists( 'grossnet_add_plugin_page_settings_link' ) ) {
+	add_filter('plugin_action_links_'.plugin_basename(__FILE__), 'grossnet_add_plugin_page_settings_link');
+	function grossnet_add_plugin_page_settings_link( $links ) {
+		$links[] = '<a href="' .
+			admin_url( 'options-general.php?page=grossnet-settings' ) .
+			'">' . __('Settings') . '</a>';
+		return $links;
+	}
+}
